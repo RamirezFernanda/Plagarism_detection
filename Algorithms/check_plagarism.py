2991ccd1d61065b0eeac92eea5da78a1ed9473f0
MelissaGR65 <82400485+MelissaGR65@users.noreply.github.com>
2023-05-05 02:17:41 -0600

    Se documentó el código

--- Algorithms/check_plagarism.py
+++ Algorithms/check_plagarism.py
@@ -3,47 +3,67 @@ Authors: Maria Fernanda Ramirez Barragan,
          Melissa Garduño Ruiz
 Creation date: 23/04/23  dd/mm/yy
 '''
-
+# The necessary libraries are imported
 import os  # For file management
 import numpy as np  # For numerical computations
 from joblib import Parallel, delayed  # For parallel processing
 from nltk.tokenize import word_tokenize  # For natural language processing
 from nltk.stem import WordNetLemmatizer, PorterStemmer  # For lemmatization and stemming
 from sklearn.feature_extraction.text import TfidfVectorizer  # For machine learning
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn import svm
-import nltk
+from sklearn.metrics.pairwise import cosine_similarity # For Cosine Similarity
+from sklearn import svm # Support vector machine (SVM) algorithm from the scikit-learn library
+import nltk # Natural Language Toolkit (NLTK)  for processing and analyzing text data
+
+#Pre-trained sentence tokenizer to split text into individual sentences
 nltk.download('punkt')
+# Downloads the WordNet corpus from the Natural Language Toolkit (nltk) library
 nltk.download('wordnet')
 
+#Two directories are specified
+#For the original documents
 original_route = "./Detection_Files/Originals"
+#For the suspicious documents
 suspicious_route = "./Detection_Files/Suspicious"
 
+#The list of original and suspicious files is obtained
 original_files = os.listdir(original_route)
 suspicious_files = os.listdir(suspicious_route)
 
+# Function that takes a document (a string) and an optional boolean parameter "stem"
+#If stem is True applies stemming to each token in the tokenized document.
 def stemming(doc, stem=False):
-    tokens = word_tokenize(doc.lower())
+    tokens = word_tokenize(doc.lower()) # Tokenizes the document and the converts alll tokens to lowercase
     if stem:
         stemmer = PorterStemmer()
         tokens = [stemmer.stem(token) for token in tokens]
+# The stemmed tokens are then joined together into a string with spaces between them and returned as the output of the function
     return " ".join(tokens)
 
+# Takes in two documents 
 def compare_files(doc1, doc2, ngram, stem=True):
 
+# And applies stemming to them if stem is set to True
     doc1_stem = stemming(doc1, stem=stem)
     doc2_stem = stemming(doc2, stem=stem)
-   
+ # Generate document vectors using TfidfVectorizer with ngram   
     vectorizer = TfidfVectorizer(ngram_range=(
-        ngram, ngram), tokenizer=word_tokenize)  # Generate document vectors
+        ngram, ngram), tokenizer=word_tokenize) 
     vectorize_doc1 = vectorizer.fit_transform([doc1_stem])
     vectorize_doc2 = vectorizer.transform([doc2_stem])
+#  Returns the similarity score
     return cosine_similarity(vectorize_doc1, vectorize_doc2)[0][0]
 
 
 
 def read_documents():
+    # Performs pairwise document comparision between all suspicious and original documents and 
+    # generates a similarity matrix containing the similarity scrores between each pair of documents
     similarities = np.zeros((len(suspicious_files), len(original_files)))
+    # The first loop iterates each suspicious document, and the second loop
+    # iterates over each original document
+    
+    #For each pair of documents, the text content of each document is read in and passsed
+    # to the function to calculate the similarity score
     for i, plagiarized_file in enumerate(suspicious_files):
         sus_route = os.path.join(suspicious_route, plagiarized_file)
         with open(sus_route, 'r', encoding="utf-8") as plagiarized_file:
@@ -58,28 +78,37 @@ def read_documents():
     return similarities
 
 
-
+# Trains an SVM model for classifying suspicious files as plagiarized or not based on 
+# their similarity scores with respect to the original files
 def train_svm_model(similarities):
     # Creating the target vector
+    # Indicates whether each suspicious file is plagiarized or not based on a 
+    # threshold of 0.13 for the similarity score
     target = np.zeros((len(suspicious_files), len(original_files)))
     for i in range(len(suspicious_files)):
         target[i, :] = (similarities[i, :] > 0.13).astype(int)
     
-    # Creating the feature matrix
+    # Creates the feature matrix containing  the similarity sccores
+    #flattened  into a single column.
     features = similarities.reshape(-1, 1)
     
-    # Training the SVM model
+    # The function trains the SVM model with a linear kernel using the feature matrix
+    #  and the target vector.
     clf = svm.SVC(kernel='linear')
     clf.fit(features, target.reshape(-1))
-    
+
+    # The trained model is returned as the output of the function
     return clf
 
+# Takes in the trained SVM model and a matrix of similarities 
 def predict_plagiarism(clf, similarities):
-    # Creating the feature matrix
+    # Creates a feature matrix by reshaping the similarities matrix into a 1D array
     features = similarities.reshape(-1, 1)
     
-    # Predicting the target vector
+    #  It uses the SVM model to predict the target vector using the feature matrix
     target = clf.predict(features)
+    # The predicted target vector is reshaped to have the same dimensions as the original
+    #  similarities matrix and returned
     target = target.reshape(len(suspicious_files), len(original_files))
     
     return target
--- run.py
+++ run.py
@@ -3,17 +3,23 @@ Authors: Maria Fernanda Ramirez Barragan,
          Melissa Garduño Ruiz
 Creation date: 23/04/23  dd/mm/yy
 '''
-
+# Imports specific functions and variables from the module "check_plagiarism" located in the "Algorithms" directory
 from Algorithms.check_plagarism import read_documents, suspicious_files, original_files, train_svm_model, predict_plagiarism
 
 
-
+# Checks if the script is being executed as the main program
 if __name__ == '__main__':
 
+# Checks for  plagiarism by calling the read_documents function to get the similarity scores between all suspicious 
+# files and all original files
     similarities = read_documents()
+# Trains the SVM model using these similarity scores and makes predictions about which pairs of files are likely to be plagiarized 
     clf = train_svm_model(similarities)
+# Returns a matrix of predicted plagiarism, where each element of the matrix represents the predicted label (0 or 1) for the corresponding 
+# suspicious and original document pair.
     predictions = predict_plagiarism(clf, similarities)
-    
+ # Loops through all pairs of suspicious and original files and prints a message if plagiarism is detected, along with the cosine 
+ # similarity score between the two documents.  
     for i, plagiarized_file in enumerate(suspicious_files):
         for j, original_file in enumerate(original_files):
             if predictions[i, j] == 1:
