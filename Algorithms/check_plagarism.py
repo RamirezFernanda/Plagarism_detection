622fc88b7f3e39bce0dbcdb84b1a8ed5b188f967
MelissaGR65 <82400485+MelissaGR65@users.noreply.github.com>
2023-05-05 00:27:12 -0600

    update check plagarism

--- Algorithms/check_plagarism.py
+++ Algorithms/check_plagarism.py
@@ -4,7 +4,6 @@ Authors: Maria Fernanda Ramirez Barragan,
 Creation date: 23/04/23  dd/mm/yy
 '''
 
-# The necessary libraries are imported
 import os  # For file management
 import numpy as np  # For numerical computations
 from joblib import Parallel, delayed  # For parallel processing
@@ -12,22 +11,16 @@ from nltk.tokenize import word_tokenize  # For natural language processing
 from nltk.stem import WordNetLemmatizer, PorterStemmer  # For lemmatization and stemming
 from sklearn.feature_extraction.text import TfidfVectorizer  # For machine learning
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn import svm
 import nltk
-# Pre-trained sentence tokenizer to split text into individual sentences
 nltk.download('punkt')
 nltk.download('wordnet')
 
-# Two directories are specified
-# For the original documents
 original_route = "./Detection_Files/Originals"
-
-# For the suspicious documents
 suspicious_route = "./Detection_Files/Suspicious"
-# The list of original and suspicious files is obtained
+
 original_files = os.listdir(original_route)
 suspicious_files = os.listdir(suspicious_route)
-
-# Function that takes two documents as input and returns their cosine similarity score
 
 def stemming(doc, stem=False):
     tokens = word_tokenize(doc.lower())
@@ -37,10 +30,10 @@ def stemming(doc, stem=False):
     return " ".join(tokens)
 
 def compare_files(doc1, doc2, ngram, stem=True):
-#  Preprocess documents by tokenizing and stemming their words, and joining them back into strings
+
     doc1_stem = stemming(doc1, stem=stem)
     doc2_stem = stemming(doc2, stem=stem)
-    # Vectorizer extracts all possible n-grams (sequences of n consecutive words) from the text
+   
     vectorizer = TfidfVectorizer(ngram_range=(
         ngram, ngram), tokenizer=word_tokenize)  # Generate document vectors
     vectorize_doc1 = vectorizer.fit_transform([doc1_stem])
@@ -48,12 +41,9 @@ def compare_files(doc1, doc2, ngram, stem=True):
     return cosine_similarity(vectorize_doc1, vectorize_doc2)[0][0]
 
 
+
 def read_documents():
-    # Performs pairwise document comparison between all suspicious and original documents and generates
-    #  a similarity matrix containing the similarity scores between each pair of documents
     similarities = np.zeros((len(suspicious_files), len(original_files)))
-    # The first loop iterates over each suspicious document, and the second loop iterates over each original document
-    # For each pair of documents, the text content of each document is read in and passed to the  function to calculate the similarity score
     for i, plagiarized_file in enumerate(suspicious_files):
         sus_route = os.path.join(suspicious_route, plagiarized_file)
         with open(sus_route, 'r', encoding="utf-8") as plagiarized_file:
@@ -66,3 +56,30 @@ def read_documents():
             similarities[i, j] = compare_files(
                 original_text, plagiarized_text, 3)
     return similarities
+
+
+
+def train_svm_model(similarities):
+    # Creating the target vector
+    target = np.zeros((len(suspicious_files), len(original_files)))
+    for i in range(len(suspicious_files)):
+        target[i, :] = (similarities[i, :] > 0.13).astype(int)
+    
+    # Creating the feature matrix
+    features = similarities.reshape(-1, 1)
+    
+    # Training the SVM model
+    clf = svm.SVC(kernel='linear')
+    clf.fit(features, target.reshape(-1))
+    
+    return clf
+
+def predict_plagiarism(clf, similarities):
+    # Creating the feature matrix
+    features = similarities.reshape(-1, 1)
+    
+    # Predicting the target vector
+    target = clf.predict(features)
+    target = target.reshape(len(suspicious_files), len(original_files))
+    
+    return target
--- run.py
+++ run.py
@@ -4,17 +4,18 @@ Authors: Maria Fernanda Ramirez Barragan,
 Creation date: 23/04/23  dd/mm/yy
 '''
 
-from Algorithms.check_plagarism import read_documents, suspicious_files, original_files
-from sklearn.metrics import roc_auc_score
+from Algorithms.check_plagarism import read_documents, suspicious_files, original_files, train_svm_model, predict_plagiarism
+
 
 
 if __name__ == '__main__':
-    # Get the similarity matrix between all suspicious and original documents
+
     similarities = read_documents()
-    # Output the documents with similarity scores above 0.30
+    clf = train_svm_model(similarities)
+    predictions = predict_plagiarism(clf, similarities)
+    
     for i, plagiarized_file in enumerate(suspicious_files):
         for j, original_file in enumerate(original_files):
-            if similarities[i, j] > 0.30:
-                # The output includes the name of the original and suspicious document, as well as the similarity score
+            if predictions[i, j] == 1:
                 print(f"¡Alerta! Se ha detectado plagio en el documento {plagiarized_file}. \nLa similitud de coseno entre el documento original {original_file} y el documento plagiado {plagiarized_file} es: {similarities[i,j]*100:.2f}%")
-                print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                print('--------------------------------------------------------------------------------------------------------')
